e7e4b3eb8f1420ea3dbecd675fa3d2b0220495c1
Iulia Mihaicuta <Iulia.Mihaicuta@softwire.com>
2025-07-04 15:22:15 +0300

    add normal items tests

---
+++ test/golden-master-text-test.spec.ts
@@ -0,0 +1,69 @@
+import { Item, GildedRose } from '../app/gilded-rose';
+import {expect} from "chai";
+
+describe('Gilded Rose', function () {
+
+    it('Normal item', function() {
+        const sellIn = 10;
+        const quality = 30;
+        const daysPassed = 5;
+
+        const gildedRose = new GildedRose([ new Item('item', sellIn, quality) ]);
+
+        for (let day = 1; day < daysPassed; day++) {
+            gildedRose.updateQuality();
+        }
+
+        const items = gildedRose.updateQuality();
+
+        expect(items[0].name).to.equal('item');
+        expect(items[0].sellIn).to.equal(sellIn - daysPassed);
+        expect(items[0].quality).to.equal(Math.max(0, quality - daysPassed));
+    });
+
+});
+
+describe('Gilded Rose', function () {
+
+    it('Normal item - check quality limit', function() {
+        const sellIn = 10;
+        const quality = 2;
+        const daysPassed = 5;
+
+        const gildedRose = new GildedRose([ new Item('item', sellIn, quality) ]);
+
+        for (let day = 1; day < daysPassed; day++) {
+            gildedRose.updateQuality();
+        }
+
+        const items = gildedRose.updateQuality();
+
+        expect(items[0].name).to.equal('item');
+        expect(items[0].sellIn).to.equal(sellIn - daysPassed);
+        expect(items[0].quality).to.equal(0);
+    });
+
+});
+
+describe('Gilded Rose', function () {
+
+    it('Normal item - check quality after sell by date', function() {
+        // after sell by date, the quality degrades twice as fast
+        const sellIn = 3;
+        const quality = 30;
+        const daysPassed = 5;
+
+        const gildedRose = new GildedRose([ new Item('item', sellIn, quality) ]);
+
+        for (let day = 1; day < daysPassed; day++) {
+            gildedRose.updateQuality();
+        }
+
+        const items = gildedRose.updateQuality();
+
+        expect(items[0].name).to.equal('item');
+        expect(items[0].sellIn).to.equal(sellIn - daysPassed);
+        expect(items[0].quality).to.equal(23);
+    });
+
+});
